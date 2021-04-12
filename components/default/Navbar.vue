fbd940de12a8e4f85d84f3e3a73dd95b19842ede
jakhongir777 <jakhogirmirzo.tursunaliev@mail.ru>
2021-04-13 00:21:47 +0500

    navbar done

--- components/default/Navbar.vue
+++ components/default/Navbar.vue
@@ -1,6 +1,11 @@
 <template>
   <div class='navbar__container'>
     <div class='navbar__list'>
+      <div class='navbar__item__lang__box'>
+        <nuxt-link :class="{active: $i18n.locale === 'en'}" class='navbar__item__lang' :to="switchLocalePath('en')">EN</nuxt-link>
+        <nuxt-link :class="{active: $i18n.locale === 'ru'}" class='navbar__item__lang' :to="switchLocalePath('ru')">RU</nuxt-link>
+        <nuxt-link :class="{active: $i18n.locale === 'uz'}" class='navbar__item__lang' :to="switchLocalePath('uz')">UZ</nuxt-link>
+      </div>
       <nuxt-link :to="localePath('/')" class='navbar__item'><span>Home</span><span><Home /></span></nuxt-link>
       <nuxt-link :to="localePath('/')" class='navbar__item'><span>About</span><span><About /></span></nuxt-link>
       <nuxt-link :to="localePath('/')" class='navbar__item'><span>Portfolio</span><span><Portfolio /></span></nuxt-link>
